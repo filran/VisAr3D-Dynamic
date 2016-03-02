132f5957ea47ca90dabfaf4d5794721ee218a7f4
Filipe Arantes Fernandes <filran@gmail.com>
2016-03-02 13:55:08 -0300

    OO Concetps

---
+++ Visual Studio Projects/OOConcepts/OOConcepts/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOConcepts
+{
+    class Car
+    {
+        public IList<Wheel> wheels = new List<Wheel>();
+
+        public Car()
+        {
+            
+        }
+    }
+}
