ed7993d419cd5c6076f81c082d36bb7f3f6e1ebf
Filipe Arantes Fernandes <filran@gmail.com>
2016-03-02 17:45:10 -0300

    refresh

--- Unity Projects/Prototypes/Prototype2/Assets/Scripts/VisualStudio/ConsoleApplication2/ConsoleApplication2/Class.cs
+++ Unity Projects/Prototypes/Prototype2/Assets/Scripts/VisualStudio/ConsoleApplication2/ConsoleApplication2/Class.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 //namespace ConsoleApplication2
 //{
@@ -12,6 +12,31 @@ using System.Threading.Tasks;
         public String Visibility { get; set; }
         public List<Attribute> Attributes = new List<Attribute>();
         public List<Method> Methods = new List<Method>();
+
+		public void addAttribute(Dictionary<String, String> attributes)
+		{
+			foreach (var att in attributes)
+			{
+				switch (att.Key)
+				{
+				case "xmi:id":
+					this.Id = att.Value;
+					break;
+					
+				case "name":
+					this.Name = att.Value;
+					break;
+					
+				case "visibility":
+					this.Visibility = att.Value;
+					break;
+					
+				default:
+					break;
+				}
+				
+			}
+		}
 
     }
 //}
