3f79d2bec769a19bba84e7514f28e85efd4a1fa9
Filipe Arantes Fernandes <filran@gmail.com>
2016-03-15 21:48:45 -0300

    Unity: Switching scenes

---
+++ Unity Projects/VisAr3D-Dynamic/Assets/Scripts/3D/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string GoToScene { get; set; }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(GoToScene);
+        //Application.LoadLevelAdditive(GoToScene);
+        //Application.UnloadLevel(Application.loadedLevelName);
+    }
+}
---
+++ Unity Projects/VisAr3D-Dynamic/Assets/Scripts/VisAr3D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts
+{
+    public class VisAr3D : MonoBehaviour
+    {
+        public string XMIFile;
+        public GameObject Package;
+        private float dist = .5f;
+
+        void Start()
+        {
+            TheCore VisAr3D = new TheCore(XMIFile);
+
+            float x = 0;
+            foreach (Package p in VisAr3D.Packages)
+            {
+                GameObject package = (GameObject)Instantiate(Package, new Vector3(x, 0, 0), Quaternion.identity);
+                Transform textPackage = package.transform.FindChild("name");
+
+                package.name = p.Id;
+                package.AddComponent<ChangeScene>().GoToScene = "PackageOpened";
+                textPackage.GetComponent<TextMesh>().name = p.Name;
+                textPackage.GetComponent<TextMesh>().text = p.Name;
+                
+                x = ++dist;
+                DontDestroyOnLoad(package);
+            }
+        }
+    }
+}
